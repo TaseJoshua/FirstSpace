b2571692c6394fe4b12bc0126409be853a1234a0
Erutase Akpobome <erutase.akpobome@liverpoolft.nhs.uk>
2021-06-30 21:02:21 +0100

    Second change

--- FirstGitHubHelloWorldApp/FirstGitHubHelloWorldApp/Program.cs
+++ FirstGitHubHelloWorldApp/FirstGitHubHelloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace FirstGitHubHelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("There has been an Awakening");
+            Console.WriteLine("This is my second change");
             Console.ReadKey();
         }
     }
