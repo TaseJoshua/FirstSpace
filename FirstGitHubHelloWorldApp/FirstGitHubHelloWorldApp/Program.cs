e0f8c21a22e22d88cb2af8f380cd27f49f222a7c
Erutase Akpobome <erutase.akpobome@liverpoolft.nhs.uk>
2021-06-30 20:48:59 +0100

    My first commit from VS to GitHub in my AWAKENING

---
+++ FirstGitHubHelloWorldApp/FirstGitHubHelloWorldApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstGitHubHelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There has been an Awakening");
+            Console.ReadKey();
+        }
+    }
+}
